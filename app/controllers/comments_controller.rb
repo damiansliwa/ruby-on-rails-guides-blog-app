d3b73984b519418045036cd37b09309ef44cd01d
damiansliwa <damian.sliwa.90@gmail.com>
2018-11-29 16:44:30 +0100

    Correct wrong method name (destroy).

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
   
   http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
-  
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
-  def delete
+  def destroy
     @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
